2ba30ff8a566f0d089ee2a41ebef75835c84a0eb
Aarya Chaudhary <aaryacdy7276@gmail.com>
2022-11-09 22:54:22 +0545

    initial commit

--- src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
+++ src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
@@ -21,16 +21,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User saveUser(User user) {
+        log.debug("Saving new user to the database.");
         return userRepo.save(user);
     }
 
     @Override
     public UserRole saveUserRole(UserRole userRole) {
+        log.debug("Saving new role to database {} "+userRole.getName());
         return roleRepo.save(userRole);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.debug("Add new role {} to user {}", username, roleName);
         User user = userRepo.findByUsername(username);
         UserRole userRole = roleRepo.findByName(roleName);
         user.getRole().add(userRole);
@@ -38,11 +41,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String username) {
+        log.debug("Fetching user {}",username);
         return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> getAllUser() {
+        log.debug("Fetching all users.");
         return userRepo.findAll();
     }
 }
